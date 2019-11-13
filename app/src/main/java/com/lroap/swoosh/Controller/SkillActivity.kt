c825d9f92dbb13e5e8c4f86734a1a35b0ec8e348
lauraalexandraroa <laualexandrar@gmail.com>
2019-11-12 23:16:58 -0800

    lesson 8

--- app/src/main/java/com/lroap/swoosh/Controller/SkillActivity.kt
+++ app/src/main/java/com/lroap/swoosh/Controller/SkillActivity.kt
@@ -2,12 +2,14 @@ package com.lroap.swoosh.Controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import com.lroap.swoosh.Model.Player
 import com.lroap.swoosh.R
 import com.lroap.swoosh.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_skill.*
+import kotlin.math.E
 
 class SkillActivity : BaseActivity() {
 
@@ -15,10 +17,22 @@ class SkillActivity : BaseActivity() {
 //    var skill = ""
         lateinit var player : Player
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
        player = intent.getParcelableExtra(EXTRA_PLAYER)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?){
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null){
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)!!
+        }
     }
 
     fun onBallerClick(view: View){
